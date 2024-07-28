fb751d396b889d7fd0a7e4cb69a7619a08fd46ae
Mcsvip <felicitycfgreene@gmail.com>
2024-07-28 20:04:32 +0800

    create about and not found page

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const stack = computed(() => {
+    return store.state.stack
+})
+</script>
+
+<template>
+    <main class="p-24 text-center">
+      <h1>Website announcement moudle</h1>
+      <h1>Website announcement moudle</h1>
+      <h1>Website announcement moudle</h1>
+    </main>
+</template>
